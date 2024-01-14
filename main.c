67d06682dd6d1be0c82a1f721f00b1c17bc27b47
dmonti <dravenmonti@gmail.com>
2024-01-14 00:28:43 -0500

    Partially fix code quality, as noted in https://git.suckless.org/sites/commit/9a788d5e9f31c541af8d580161f3033c6b4d41c1.html . There are probably other issues that were not documented in the feedback

--- main.c
+++ main.c
@@ -3,11 +3,9 @@
 #include <sys/stat.h>
 #include <dirent.h>
 #include <string.h>
+#include <stdlib.h>
 
-#define MAX_PATH 16384
-#define MAX_TAG 1024
-#define MAX_TAGS 1024
-#define MAX_DIR 1024
+#define MAX_TEXT 4096
 
 #define HEADER "<!DOCTYPE html> <html><head><link rel='stylesheet' href='/main.css'><meta name='viewport' content='width=device-width, initial-scale=1'></head><body><article>"
 
@@ -15,67 +13,85 @@
 
 #define FIND_MATCH(match, str) \
     } else if (strcmp(text+1,match) == 0) { \
-        fgets(text, MAX_PATH, inFile); \
-        strchr(text,'\n')[0] = '\0'; \
-        snprintf(outText,MAX_PATH,str,text,text); \
+        fgets(text, MAX_TEXT, inFile); \
+        char *ln = strchr(text,'\n'); \
+        if (!ln) break; \
+        ln[0] = '\0'; \
+        snprintf(outText,MAX_TEXT,str,text,text); \
 
 struct tag {
-    char tag[MAX_TAG];
-    char body[MAX_PATH];
+    char tag[MAX_TEXT];
+    char body[MAX_TEXT];
 };
 
-struct tag tags[MAX_TAGS];
+struct tag *tags;
 
 struct tag *findTag(char *tag) {
-    for (int i = 0; i < MAX_TAGS; i++) {
+    for (int i = 0; i < MAX_TEXT; i++) {
         if (strcmp(tags[i].tag,tag) == 0) return &tags[i];
         if (tags[i].tag[0] == '\0') {
             strcpy(tags[i].tag,tag);
-            snprintf(tags[i].body,MAX_PATH,"<h1>Tag @%s</h1>",tag);
+            snprintf(tags[i].body,MAX_TEXT,"<h1>Tag @%s</h1>",tag);
             return &tags[i];
         }
     }
 }
 
 void gen(char *basePath, char *inPathI, char *outPathI) {
-    char outPath[MAX_PATH], inPath[MAX_PATH], text[MAX_PATH], outText[MAX_PATH], title[MAX_PATH], desc[MAX_PATH];
-    int matchTotal = 0;
-    snprintf(outPath,MAX_PATH,"%s/!%s.html",outPathI,basePath);
-    snprintf(inPath,MAX_PATH,"%s/%s",inPathI,basePath);
+    char *path, *text, *outText, *title, *desc;
 
-    FILE *inFile = fopen(inPath,"r");
-    FILE *outFile = fopen(outPath,"w");
+    // this is probably awful
+    path = malloc(PATH_MAX);
+    text = malloc(MAX_TEXT);
+    outText = malloc(MAX_TEXT);
+    title = malloc(MAX_TEXT);   
+    desc = malloc(MAX_TEXT);
+    
+    int matchTotal = 0;
+    snprintf(path,PATH_MAX,"%s/!%s.html",outPathI,basePath);
+    
+    FILE *inFile = fopen(path,"r");
+    
+    snprintf(path,PATH_MAX,"%s/%s",inPathI,basePath);
+    FILE *outFile = fopen(path,"w");
+
+    free(path);
     
     if (!inFile || !outFile) return;
 
     fputs(HEADER,outFile);
 
-    while (fgets(text, MAX_PATH, inFile)) {
-        snprintf(outText,MAX_PATH,"%s",text);
+    while (fgets(text, MAX_TEXT, inFile)) {
+        snprintf(outText,MAX_TEXT,"%s",text);
 
         if (text[0] != '#') {
-            snprintf(outText,MAX_PATH,"%s<p>%s</p>",outText,text);
+            snprintf(outText,MAX_TEXT,"%s<p>%s</p>",outText,text);
 
             FIND_MATCH("TITLE\n","<h1>%s</h1>")
-            snprintf(title,MAX_PATH,"%s",text);
+            snprintf(title,MAX_TEXT,"%s",text);
 
             FIND_MATCH("TAG\n","<a href='@%s.html'>@%s</a>")
             struct tag *found = findTag(text);          
             char *body = found->body;
-            snprintf(strchr(body,'\0'),MAX_PATH,"<a href='!%s.html'><h3>%s</h3></a><p><i>%s</i></p></p>\n", basePath, title, desc);
+            snprintf(strchr(body,'\0'),MAX_TEXT,"<a href='!%s.html'><h3>%s</h3></a><p><i>%s</i></p></p>\n", basePath, title, desc);
 
             FIND_MATCH("DESC\n","<i>%s</i>");
-            snprintf(desc,MAX_PATH,"%s",text);
+            snprintf(desc,MAX_TEXT,"%s",text);
         }
 
         fputs(outText,outFile);
     }
 
     fputs(FOOTER,outFile);
+
+    free(text);
+    free(outText);
+    free(title);
+    free(desc);
 }
 
 void list(char *basePath, char *outPath) {
-    char path[MAX_DIR];
+    char *path = malloc(PATH_MAX);
     struct dirent *dp;
     DIR *dir = opendir(basePath);
 
@@ -94,29 +110,38 @@ void list(char *basePath, char *outPath) {
     }
 
     closedir(dir);
+
+    free(path);
 }
 
 void tag(char *outPathI) {
-    for (int i = 0; i < MAX_TAGS; i++) {
-        char outPath[MAX_PATH];
+    tags = malloc(sizeof(struct tag) * MAX_TEXT);
+
+    for (int i = 0; i < MAX_TEXT; i++) {
+        char *outPath = malloc(MAX_TEXT);
 
         char *tag = tags[i].tag;
         if (tag[0] == '\0') break;
         
-        snprintf(outPath,MAX_PATH,"%s/@%s.html",outPathI,tag);
+        snprintf(outPath,MAX_TEXT,"%s/@%s.html",outPathI,tag);
     
         FILE *outFile = fopen(outPath,"w");
         fputs(HEADER,outFile);
         fputs(tags[i].body,outFile);
         fputs(FOOTER,outFile);
     
+        fclose(outFile);
+    
+        free(outPath);
     }
+
+    free(tags);
 }
 
 int main(int argc, char** argv) {
     if (argc < 3) {
         printf("usage: %s [input dir] [output dir]\n",argv[0]);
-        return -1;
+        return 1;
     }
 
     mkdir(argv[2], S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
